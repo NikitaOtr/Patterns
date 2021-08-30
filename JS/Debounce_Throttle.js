c42ecee11fe8e30704cb28162565eaa12cf5dfd7
NikitaOtr <nikita137174@list.ru>
2021-08-30 13:14:20 +0300

    Debounce_Throttle

---
+++ JS/Debounce_Throttle.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const input = document.querySelector('input');
+const p = document.querySelector('p');
+
+//Debounce
+// time 250 - 300
+const debounce = (fn, time) => {
+    let flagTimeout;
+    return function(...arg) {
+        const fnCall = () => fn.apply(this, arg);
+        clearTimeout(flagTimeout);
+        flagTimeout = setTimeout(fnCall, time, arg);
+    };
+};
+
+const print = function(event) {
+    const target = event.target;
+    p.textContent = target.value;
+};
+const printDebounce = debounce(print, 300);
+input.addEventListener('input', printDebounce);
+
+
+// Throttle
+const throttle = (fn, time) => {
+    let isThrottled = false;
+    let saveThis;
+    let saveArgs;
+    return function wrapper(...arg) {
+        if (isThrottled) {
+            saveThis = this;
+            saveArgs = arg;
+            return;
+        }
+        fn.apply(this, arg);
+        isThrottled = true;
+        setTimeout(() => {
+            isThrottled = false;
+            if (saveArgs) {
+                wrapper.apply(saveThis, saveArgs);
+                saveArgs = saveThis = null;
+            }
+        }, time);
+    };
+};
+
+const consoleCoorrinate = function(event) {
+    p.textContent = event.clientX + ' ' + event.clientY;
+};
+const consoleCoorrinateThrottle = throttle(consoleCoorrinate, 1000);
+document.addEventListener('mousemove', consoleCoorrinateThrottle);
+
+
