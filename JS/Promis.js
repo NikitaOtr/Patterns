c38a8aeb76d4978f6474e802ddcf97af6dfead34
NikitaOtr <nikita137174@list.ru>
2021-09-02 19:20:35 +0300

    Promise postData

--- JS/Promis.js
+++ JS/Promis.js
@@ -1,6 +1,29 @@
 'use strict';
 const output = document.getElementById('Promise');
 
+const postData = (url, data) => new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                resolve();
+            } else {
+                reject(request.statusText);
+            }
+        }
+    });
+    request.open('POST', url);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(data));
+});
+
+postData()
+    .then(() => console.log('Всё гуд'))
+    .catch(error => console.log(error));
+
+
+
+
 const getData = url => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
     request.addEventListener('readystatechange', () => {
@@ -29,12 +52,14 @@ const oneImg = getData('https://jsonplaceholder.typicode.com/photos/1');
 const twoImg = getData('https://jsonplaceholder.typicode.com/photos/2');
 const threeImg = getData('https://jsonplaceholder.typicode.com/photos/3');
 
-// выполниться после race
+// all дождется загрузки всех и только потом выполнит then.
+// В Функцию outputPhoto попадёт [dataOneImg, dataTwoImg, dataThreeImg]
 Promise.all([oneImg, twoImg, threeImg])
     .then(outputPhoto)
     .catch(error => console.log(error));
 
-// выполниться перед all
+// race в then попадёт первый загруженны из массива.
+// В Функцию outputPhoto попадёт данные из первого занруженного
 Promise.race([oneImg, twoImg, threeImg])
     .then(data => outputPhoto([data]))
     .catch(error => console.log(error));
